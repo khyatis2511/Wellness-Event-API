e33ac0716bf51c93fded304d0e3bd6e52c60ba2c
khyati2511 <khushisoni2511@gmail.com>
2023-10-15 17:20:33 +0530

    Project Initialization

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wellness Event Web Application')
+    .setDescription('Wellness Event Web Application APIs for HR and vendor')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 3088);
+
+  console.log(`Nest App Started on ${process.env.PORT ?? 3088}`);
+  console.log(`http://localhost:${process.env.PORT ?? 3088}/api/`);
+}
+bootstrap();
