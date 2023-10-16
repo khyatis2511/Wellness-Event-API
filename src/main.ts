ce28fe840ce52a52a31f9c2df52797a67ad717ec
khyati2511 <khushisoni2511@gmail.com>
2023-10-17 03:59:09 +0530

    prisma changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaService } from './shared/modules/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   app.enableCors(); // This allows all origins, you may want to configure it more specifically in production
 
