aa961b9895ad603e3a3222c4e1d52294d6a23887
khyati2511 <khushisoni2511@gmail.com>
2023-10-17 05:32:49 +0530

    get prisma chanegs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { PrismaService } from './shared/modules/prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService);
+  app.get(PrismaService);
+
+  // const prismaService = app.get(PrismaService);
   // await prismaService.enableShutdownHooks(app);
 
   app.enableCors(); // This allows all origins, you may want to configure it more specifically in production
